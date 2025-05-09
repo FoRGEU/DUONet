fc06ec23aeaba3cc516f51ac25eb7c3505595c97
FoRGEU <141420094+FoRGEU@users.noreply.github.com>
2025-05-09 18:30:01 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python /home/quchenyu/DUONet/train_net.py --config-file /home/quchenyu/DUONet/configs/ShipRS_config_37+5.yaml
+python train_net.py --config-file configs/ShipRS_config_37+5.yaml
 
 
