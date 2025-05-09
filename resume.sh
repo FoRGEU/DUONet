835eb3fa0a231caa9efe596b867b05445b468405
FoRGEU <141420094+FoRGEU@users.noreply.github.com>
2025-05-09 18:30:56 +0800

    Update resume.sh

--- resume.sh
+++ resume.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /home/quchenyu/DUONet/train_net.py --resume --config-file /home/quchenyu/DUONet/configs/ShipRS_config_37+5.yaml
+python train_net.py --resume --config-file configs/ShipRS_config_37+5.yaml
