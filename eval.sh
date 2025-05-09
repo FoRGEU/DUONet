1d8bf72e4f2773e732a88891364efa2512cb3221
FoRGEU <141420094+FoRGEU@users.noreply.github.com>
2025-05-09 18:30:36 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python /home/quchenyu/DUONet/train_net.py --config-file /home/quchenyu/DUONet/configs/ShipRS_config_37+5.yaml  --eval-only \
-MODEL.WEIGHTS  /home/quchenyu/DUONet/output/ShipRS/model_final.pth
+python train_net.py --config-file configs/ShipRS_config_37+5.yaml  --eval-only \
+MODEL.WEIGHTS  output/ShipRS/model_final.pth
 
 
 
